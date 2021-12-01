5341be937ac9920b3a4366c0f92490bbc3fddddc
Jason Noonan <jason.noonan@pdq.com>
2021-12-01 09:54:34 -0700

    chore: implement filter solution for part 1

--- src/day1.rs
+++ src/day1.rs
@@ -6,8 +6,8 @@ pub fn input_generator(input: &str) -> Vec<i32> {
         .collect()
 }
 
-#[aoc(day1, part1)]
-pub fn part1(input: &[i32]) -> i32 {
+#[aoc(day1, part1, for_loop)]
+pub fn part1_for_loop(input: &[i32]) -> i32 {
     let mut counter = 0;
     for x in 0..input.len() {
         if x > 0 && input[x] > input[x - 1] {
@@ -15,6 +15,14 @@ pub fn part1(input: &[i32]) -> i32 {
         }
     }
     counter
+}
+
+#[aoc(day1, part1, filter)]
+pub fn part1_filter(input: &[i32]) -> i32 {
+    input
+        .windows(2)
+        .filter(|window| window[0] < window[1])
+        .count() as i32
 }
 
 #[aoc(day1, part2)]
@@ -31,6 +39,18 @@ pub fn part2(input: &[i32]) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part1_for_loop() {
+        let input = input_generator("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n");
+        assert_eq!(part1_for_loop(&input), 7);
+    }
+
+    #[test]
+    fn test_part1_filter() {
+        let input = input_generator("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n");
+        assert_eq!(part1_filter(&input), 7);
+    }
 
     #[test]
     fn test_part2() {
