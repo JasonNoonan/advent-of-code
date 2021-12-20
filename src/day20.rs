73d00a385e65815577aabd8b788848ffb27ed699
Jason Noonan <jason.noonan@pdq.com>
2021-12-20 09:58:51 -0700

    chore: Day 20 wip

---
+++ src/day20.rs
@@ -0,0 +1,128 @@
+#[derive(Debug)]
+pub struct ImageAlgorithm {
+    pub input: Vec<Vec<char>>,
+    pub algorithm: Vec<char>,
+}
+
+#[aoc_generator(day20)]
+pub fn input_generator(input: &str) -> ImageAlgorithm {
+    let mut lines = input.lines();
+    let algorithm = lines.next().unwrap();
+    let _ = input.lines().next();
+    let mut result_vec: Vec<Vec<char>> = Vec::new();
+
+    for line in lines {
+        if !line.is_empty() {
+            let mut line_vec: Vec<char> = Vec::new();
+            for c in line.chars() {
+                line_vec.push(c);
+            }
+            result_vec.push(line_vec);
+        }
+    }
+
+    ImageAlgorithm {
+        input: result_vec,
+        algorithm: algorithm.chars().collect(),
+    }
+}
+
+#[aoc(day20, part1)]
+pub fn part1(input: &ImageAlgorithm) -> usize {
+    let mut result: Vec<Vec<char>> = enhance_image(&input.input, &input.algorithm);
+    result = enhance_image(&result, &input.algorithm);
+    println!("length: {:?} x {}", result.len(), result[0].len());
+    result.iter().flatten().filter(|&c| *c == '#').count()
+}
+
+fn enhance_image(image: &[Vec<char>], alg: &[char]) -> Vec<Vec<char>> {
+    let mut result: Vec<Vec<char>> = Vec::new();
+    let image = &create_larger_frame(image);
+
+    for y in 0..image.len() {
+        let mut line_vec: Vec<char> = Vec::new();
+        for x in 0..image[y].len() {
+            let binary_value = convert_grid_to_int(image, x as isize, y as isize);
+            line_vec.push(alg[binary_value]);
+        }
+
+        result.push(line_vec);
+    }
+
+    result
+}
+
+fn create_larger_frame(image: &[Vec<char>]) -> Vec<Vec<char>> {
+    let dot_line = vec!['.'; image[0].len() + 4];
+    let mut result: Vec<Vec<char>> = vec![dot_line.clone(); 2];
+
+    for y in image {
+        let mut line_vec: Vec<char> = vec!['.'; 2];
+
+        line_vec.extend_from_slice(y);
+
+        line_vec.push('.');
+        line_vec.push('.');
+        result.push(line_vec);
+    }
+
+    result.push(dot_line.clone());
+    result.push(dot_line);
+    result
+}
+
+fn convert_grid_to_int(image: &[Vec<char>], x: isize, y: isize) -> usize {
+    let mut result_string = String::new();
+
+    result_string.push(try_get_index(image, x - 1, y - 1));
+    result_string.push(try_get_index(image, x, y - 1));
+    result_string.push(try_get_index(image, x + 1, y - 1));
+
+    result_string.push(try_get_index(image, x - 1, y));
+    result_string.push(try_get_index(image, x, y));
+    result_string.push(try_get_index(image, x + 1, y));
+
+    result_string.push(try_get_index(image, x - 1, y + 1));
+    result_string.push(try_get_index(image, x, y + 1));
+    result_string.push(try_get_index(image, x + 1, y + 1));
+
+    result_string = result_string.replace("#", "1").replace(".", "0");
+
+    usize::from_str_radix(result_string.as_str(), 2).unwrap()
+}
+
+fn try_get_index(image: &[Vec<char>], x: isize, y: isize) -> char {
+    if x < 0 || y < 0 {
+        return '.';
+    }
+
+    if let Some(c) = image.get(y as usize) {
+        if let Some(c) = c.get(x as usize) {
+            *c
+        } else {
+            '.'
+        }
+    } else {
+        '.'
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = r"..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+#..#.
+#....
+##..#
+..#..
+..###
+";
+
+    #[test]
+    fn test_part1() {
+        let input = input_generator(INPUT);
+        assert_eq!(part1(&input), 35);
+    }
+}
