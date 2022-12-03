5ba365a5e30447e4a9325a182b9e7d3af8744bf5
Jason Noonan <jason.noonan@pdq.com>
2022-12-03 03:00:55 -0600

    use to_charlist, string split instead of index_at

--- elixir/lib/advent_of_code/day_03.ex
+++ elixir/lib/advent_of_code/day_03.ex
@@ -2,7 +2,15 @@ defmodule AdventOfCode.Day03 do
   def part1(args) do
     args
     |> split_lines()
-    |> Enum.map(&sort_sacks/1)
+    |> Enum.reduce([], fn rucksack, acc ->
+      length = String.length(rucksack)
+      half = Integer.floor_div(length, 2)
+
+      {c1, c2} = String.split_at(rucksack, half)
+      diff = MapSet.intersection(to_mapset(c1), to_mapset(c2)) |> Enum.uniq() |> to_string()
+
+      [get_index_of_char(diff) | acc]
+    end)
     |> Enum.sum()
     |> dbg()
   end
@@ -11,41 +19,28 @@ defmodule AdventOfCode.Day03 do
     args
     |> String.split("\n", trim: true)
     |> Enum.chunk_every(3)
-    |> Enum.map(fn [a, b, c] ->
-      a = String.graphemes(a) |> MapSet.new()
-      b = String.graphemes(b) |> MapSet.new()
-      c = String.graphemes(c) |> MapSet.new()
+    |> Enum.reduce([], fn [a, b, c], acc ->
+      badge =
+        MapSet.intersection(to_mapset(a), MapSet.intersection(to_mapset(b), to_mapset(c)))
+        |> Enum.uniq()
+        |> to_string()
 
-      badge = MapSet.intersection(a, MapSet.intersection(b, c)) |> Enum.uniq() |> Enum.join()
-
-      get_index_of_char(badge) + 1
+      [get_index_of_char(badge) | acc]
     end)
     |> Enum.sum()
     |> dbg()
-  end
-
-  def sort_sacks(rucksack) do
-    length = String.length(rucksack)
-    char_list = String.graphemes(rucksack)
-
-    half = Integer.floor_div(length, 2)
-    c1 = Enum.take(char_list, half) |> MapSet.new()
-    c2 = Enum.take(char_list, half * -1) |> MapSet.new()
-    diff = MapSet.intersection(c1, c2) |> Enum.uniq() |> Enum.join()
-
-    get_index_of_char(diff) + 1
   end
 
   def split_lines(args) do
     args |> String.split("\n", trim: true)
   end
 
-  def split_characters(args) do
-    args |> String.graphemes()
+  def to_mapset(string) do
+    string |> String.to_charlist() |> MapSet.new()
   end
 
   def get_index_of_char(target) do
-    char_list = String.graphemes("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    Enum.find_index(char_list, fn char -> char == target end)
+    [char_list, _] = String.split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", target)
+    String.length(char_list) + 1
   end
 end
