c523ed8d4988180be755287ed10031b62e3c93ed
Jason Noonan <jason.noonan@pdq.com>
2022-12-03 01:12:00 -0600

    day 3, part 1

--- elixir/lib/advent_of_code/day_03.ex
+++ elixir/lib/advent_of_code/day_03.ex
@@ -1,7 +1,38 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> split_lines()
+    |> Enum.map(&sort_sacks/1)
+    |> Enum.sum()
+    |> dbg()
   end
 
   def part2(_args) do
+  end
+
+  def sort_sacks(rucksack) do
+    length = String.length(rucksack)
+    char_list = split_characters(rucksack)
+
+    half = Integer.floor_div(length, 2)
+    c1 = Enum.take(char_list, half)
+    c2 = Enum.take(char_list, half * -1)
+
+    not_in_list_2 = c1 -- c2
+    diff = (c1 -- not_in_list_2) |> Enum.uniq() |> Enum.join()
+    get_index_of_char(diff) + 1
+  end
+
+  def split_lines(args) do
+    args |> String.split("\n", trim: true)
+  end
+
+  def split_characters(args) do
+    args |> String.graphemes()
+  end
+
+  def get_index_of_char(target) do
+    char_list = String.graphemes("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    Enum.find_index(char_list, fn char -> char == target end)
   end
 end
--- elixir/test/advent_of_code/day_03_test.exs
+++ elixir/test/advent_of_code/day_03_test.exs
@@ -3,12 +3,19 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  vJrwpWtwJgWrhcsFMMfFFhFp
+  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+  PmmdzqPrVvPwwTWBwg
+  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+  ttgJtRGJQctTZtZT
+  CrZsJsPPZsGzwwsLwLmpwMDw
+  """
 
-    assert result
+  test "part1" do
+    result = part1(@input)
+
+    assert result == 157
   end
 
   @tag :skip
