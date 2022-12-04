198e37c27371bcc05e4283208b4412eb1a1d7fcb
Jason Noonan <jason.noonan@pdq.com>
2022-12-04 13:15:21 -0600

    use ranges and doctests

--- elixir/lib/advent_of_code/day_04.ex
+++ elixir/lib/advent_of_code/day_04.ex
@@ -1,33 +1,56 @@
 defmodule AdventOfCode.Day04 do
   def part1(args) do
     args
-    |> String.split("\n", trim: true)
-    |> Enum.reduce(0, fn curr, acc ->
-      [a, b] = String.split(curr, ",")
-      [ax, ay] = String.split(a, "-") |> Enum.map(&String.to_integer/1)
-      [bx, by] = String.split(b, "-") |> Enum.map(&String.to_integer/1)
-      a = MapSet.new(ax..ay)
-      b = MapSet.new(bx..by)
-
-      int = MapSet.intersection(a, b)
-      update = if MapSet.equal?(int, a) || MapSet.equal?(int, b), do: 1, else: 0
-      acc + update
+    |> split_to_component_sectors()
+    |> Stream.filter(fn [[a, b], [y, z]] ->
+      either_fully_contains(Range.new(a, b), Range.new(y, z))
     end)
+    |> Enum.count()
   end
 
   def part2(args) do
     args
-    |> String.split("\n", trim: true)
-    |> Enum.reduce(0, fn curr, acc ->
-      [a, b] = String.split(curr, ",")
-      [ax, ay] = String.split(a, "-") |> Enum.map(&String.to_integer/1)
-      [bx, by] = String.split(b, "-") |> Enum.map(&String.to_integer/1)
-      a = MapSet.new(ax..ay)
-      b = MapSet.new(bx..by)
-
-      update = if MapSet.size(MapSet.intersection(a, b)) > 0, do: 1, else: 0
-      acc + update
+    |> split_to_component_sectors()
+    |> Stream.filter(fn [[a, b], [y, z]] ->
+      !Range.disjoint?(Range.new(a, b), Range.new(y, z))
     end)
-    |> dbg
+    |> Enum.count()
+  end
+
+  @doc """
+  splits provided range strings into 2 pairs of start,end lists
+
+  ## Examples
+    iex> AdventOfCode.Day04.split_to_component_sectors("1-4,2-5\\n3-5,6-9\\n1-9,0-4\\n")
+    [[[1, 4], [2, 5]], [[3, 5], [6, 9]], [[1, 9], [0, 4]]]  
+  """
+  def split_to_component_sectors(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn x ->
+      String.split(x, ",")
+      |> Enum.map(fn child ->
+        String.split(child, "-")
+        |> Enum.map(&String.to_integer/1)
+      end)
+    end)
+  end
+
+  @doc """
+  returns true if either a fully contains b, or b fully contains a
+
+  ## Examples
+    iex> AdventOfCode.Day04.either_fully_contains(Range.new(1,3), Range.new(4,6))
+    false
+
+    iex> AdventOfCode.Day04.either_fully_contains(Range.new(1,3), Range.new(2,2))
+    true
+
+    iex> AdventOfCode.Day04.either_fully_contains(Range.new(1,3), Range.new(2,4))
+    false
+  """
+  def either_fully_contains(a, b) do
+    !Range.disjoint?(a, b) &&
+      (Enum.all?(a, fn i -> i in b end) || Enum.all?(b, fn i -> i in a end))
   end
 end
--- elixir/test/advent_of_code/day_04_test.exs
+++ elixir/test/advent_of_code/day_04_test.exs
@@ -1,5 +1,6 @@
 defmodule AdventOfCode.Day04Test do
   use ExUnit.Case
+  doctest AdventOfCode.Day04
 
   import AdventOfCode.Day04
 
