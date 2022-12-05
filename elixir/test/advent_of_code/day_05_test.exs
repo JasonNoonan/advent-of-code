ae3e8dcc2ff274df9671206e838967c68a845100
Jason Noonan <jason.noonan@pdq.com>
2022-12-05 12:22:24 -0600

    day 5 holy shit

--- elixir/test/advent_of_code/day_05_test.exs
+++ elixir/test/advent_of_code/day_05_test.exs
@@ -2,20 +2,29 @@ defmodule AdventOfCode.Day05Test do
   use ExUnit.Case
 
   import AdventOfCode.Day05
+  doctest AdventOfCode.Day05
 
-  @tag :skip
+  @input """
+      [D]    
+  [N] [C]    
+  [Z] [M] [P]
+   1   2   3 
+
+  move 1 from 2 to 1
+  move 3 from 1 to 3
+  move 2 from 2 to 1
+  move 1 from 1 to 2
+  """
+
   test "part1" do
-    input = nil
-    result = part1(input)
+    result = part1(@input)
 
-    assert result
+    assert result == "CMZ"
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
-    assert result
+    assert result == "MCD"
   end
 end
