fe41056c2414dd473aab09cc0a1094335ec9821a
Jason Noonan <jason.noonan@pdq.com>
2022-12-06 07:15:33 -0600

    day 06

--- elixir/lib/advent_of_code/day_06.ex
+++ elixir/lib/advent_of_code/day_06.ex
@@ -1,7 +1,65 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  @doc """
+    iex> AdventOfCode.Day06.part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+    7
+
+    iex> AdventOfCode.Day06.part1("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    5
+
+    iex> AdventOfCode.Day06.part1("nppdvjthqldpwncqszvftbrmjlhg")
+    6
+
+    iex> AdventOfCode.Day06.part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    10
+
+    iex> AdventOfCode.Day06.part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+    11
+  """
+  def part1(args) do
+    get_index(String.graphemes(args), 4)
   end
 
-  def part2(_args) do
+  @doc """
+    iex> AdventOfCode.Day06.part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+    19
+
+    iex> AdventOfCode.Day06.part2("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    23
+
+    iex> AdventOfCode.Day06.part2("nppdvjthqldpwncqszvftbrmjlhg")
+    23
+
+    iex> AdventOfCode.Day06.part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    29
+
+    iex> AdventOfCode.Day06.part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+    26
+
+  """
+  def part2(args) do
+    get_index(String.graphemes(args), 14)
+  end
+
+  @doc """
+    iex> AdventOfCode.Day06.get_index(String.graphemes("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 4)
+    7
+
+    iex> AdventOfCode.Day06.get_index(String.graphemes("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 14)
+    19
+  """
+  def get_index(input, size) do
+    indexes =
+      for {_, index} <- input |> Enum.with_index(),
+          slice = Enum.slice(input, index, size),
+          sorted = Enum.sort(slice),
+          dedupe = Enum.dedup(sorted),
+          reduce: [] do
+        acc ->
+          if Enum.count(dedupe) == size,
+            do: [index + size | acc],
+            else: acc
+      end
+
+    List.first(Enum.reverse(indexes))
   end
 end
--- elixir/test/advent_of_code/day_06_test.exs
+++ elixir/test/advent_of_code/day_06_test.exs
@@ -2,19 +2,18 @@ defmodule AdventOfCode.Day06Test do
   use ExUnit.Case
 
   import AdventOfCode.Day06
+  doctest AdventOfCode.Day06
 
   @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    result = part1(@input)
 
     assert result
   end
 
   @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
     assert result
   end
