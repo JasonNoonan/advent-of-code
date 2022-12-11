4c0db29762cd180c7181bc770901c57b6ae50466
Jason Noonan <jason.noonan@pdq.com>
2022-12-11 01:06:47 -0600

    day 11 part 1

--- elixir/lib/advent_of_code/day_11.ex
+++ elixir/lib/advent_of_code/day_11.ex
@@ -1,5 +1,98 @@
 defmodule AdventOfCode.Day11 do
-  def part1(_args) do
+  @monkey_parse_regex ~r/Monkey \d+:\n\W*Starting items: (?<starting_items>.*)\n\W*Operation: new = old (?<operation>.) (?<modifier>\w+)\n\W*Test: divisible by (?<divider>\w*)\n\W*If true: throw to monkey (?<true_target>\w*)\n\W*If false: throw to monkey (?<false_target>\w*)/
+
+  def parse(args) do
+    monkeys = String.split(args, "\n\n", trim: true)
+
+    monkey_list =
+      for monkey <- monkeys do
+        base = Regex.named_captures(@monkey_parse_regex, monkey)
+
+        %{
+          starting_items: get_starting_items_list(base["starting_items"]),
+          operation: base["operation"],
+          modifier: base["modifier"],
+          divider: String.to_integer(base["divider"]),
+          true_target: String.to_integer(base["true_target"]),
+          false_target: String.to_integer(base["false_target"]),
+          count: 0
+        }
+      end
+
+    monkey_list
+  end
+
+  def get_starting_items_list(items) do
+    String.split(items, ", ")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def inspect(worry_level, operation, modifier, lower_worry?) do
+    modifier = if modifier == "old", do: worry_level, else: String.to_integer(modifier)
+
+    during_inspection =
+      if operation == "*", do: worry_level * modifier, else: worry_level + modifier
+
+    worry_level = if lower_worry?, do: floor(during_inspection / 3), else: during_inspection
+    worry_level
+  end
+
+  def test_item(worry_level, divider) do
+    rem(worry_level, divider) == 0
+  end
+
+  def throw_to_target(worry_level, target) do
+    list = target.starting_items |> Enum.reverse()
+    [worry_level | list] |> Enum.reverse()
+  end
+
+  def calculate_round(monkeys, rounds) when rounds > 0 do
+    monkeys =
+      for i <- 0..(length(monkeys) - 1), reduce: monkeys do
+        outer_acc ->
+          monkey = Enum.at(outer_acc, i)
+          count = length(monkey.starting_items)
+
+          outer_acc =
+            for item <- monkey.starting_items,
+                after_inspection = inspect(item, monkey.operation, monkey.modifier, true),
+                reduce: outer_acc do
+              acc ->
+                target_index =
+                  if test_item(after_inspection, monkey.divider),
+                    do: monkey.true_target,
+                    else: monkey.false_target
+
+                target = Enum.at(acc, target_index)
+                target_items = throw_to_target(after_inspection, target)
+                updated_target = Map.put(target, :starting_items, target_items)
+                acc = List.replace_at(acc, target_index, updated_target)
+                acc
+            end
+
+          updated =
+            Map.put(monkey, :starting_items, [])
+            |> Map.update(:count, count, fn curr -> curr + count end)
+
+          outer_acc = List.replace_at(outer_acc, i, updated)
+          outer_acc
+      end
+
+    calculate_round(monkeys, rounds - 1)
+  end
+
+  def calculate_round(monkeys, _), do: monkeys
+
+  def part1(args) do
+    args
+    |> parse()
+    |> calculate_round(20)
+    |> Enum.map(fn x -> x.count end)
+    |> Enum.sort(:desc)
+    |> Enum.take(2)
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [x, y] -> x * y end)
+    |> List.first()
   end
 
   def part2(_args) do
--- elixir/test/advent_of_code/day_11_test.exs
+++ elixir/test/advent_of_code/day_11_test.exs
@@ -3,12 +3,83 @@ defmodule AdventOfCode.Day11Test do
 
   import AdventOfCode.Day11
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  Monkey 0:
+    Starting items: 79, 98
+    Operation: new = old * 19
+    Test: divisible by 23
+      If true: throw to monkey 2
+      If false: throw to monkey 3
 
-    assert result
+  Monkey 1:
+    Starting items: 54, 65, 75, 74
+    Operation: new = old + 6
+    Test: divisible by 19
+      If true: throw to monkey 2
+      If false: throw to monkey 0
+
+  Monkey 2:
+    Starting items: 79, 60, 97
+    Operation: new = old * old
+    Test: divisible by 13
+      If true: throw to monkey 1
+      If false: throw to monkey 3
+
+  Monkey 3:
+    Starting items: 74
+    Operation: new = old + 3
+    Test: divisible by 17
+      If true: throw to monkey 0
+      If false: throw to monkey 1
+  """
+
+  test "monkey parser" do
+    result = parse(@input)
+
+    assert result == [
+             %{
+               divider: 23,
+               false_target: 3,
+               modifier: "19",
+               operation: "*",
+               starting_items: 'Ob',
+               true_target: 2,
+               count: 0
+             },
+             %{
+               divider: 19,
+               false_target: 0,
+               modifier: "6",
+               operation: "+",
+               starting_items: '6AKJ',
+               true_target: 2,
+               count: 0
+             },
+             %{
+               divider: 13,
+               false_target: 3,
+               modifier: "old",
+               operation: "*",
+               starting_items: 'O<a',
+               true_target: 1,
+               count: 0
+             },
+             %{
+               divider: 17,
+               false_target: 1,
+               modifier: "3",
+               operation: "+",
+               starting_items: 'J',
+               true_target: 0,
+               count: 0
+             }
+           ]
+  end
+
+  test "part1" do
+    result = part1(@input)
+
+    assert result == 10_605
   end
 
   @tag :skip
