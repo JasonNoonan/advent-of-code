d1d79685d2bb1d8f46245b15e1edb5f4f50d906b
Jason Noonan <jason.noonan@pdq.com>
2022-12-13 20:06:36 -0600

    break apart pairs

--- elixir/lib/advent_of_code/day_13.ex
+++ elixir/lib/advent_of_code/day_13.ex
@@ -1,24 +1,26 @@
 defmodule AdventOfCode.Day13 do
   @doc """
-    # iex> AdventOfCode.Day13.parse("[1,1,3,1,1]\\n[1,1,5,1,1]\\n\\n[[1],[2,3,4]]\\n[[1],4]")
-    # [[[1,1,3,1,1], [1,1,5,1,1]], [[[1],[2,3,4]], [[1], 4]]]
+    iex> AdventOfCode.Day13.parse("[1,1,3,1,1]\\n[1,1,5,1,1]\\n\\n[[1],[2,3,4]]\\n[[1],4]")
+    [[1,1,3,1,1], [1,1,5,1,1], [[1],[2,3,4]], [[1], 4]]
   """
   def parse(args) do
     args
     |> String.split("\n\n", trim: true)
     |> Enum.map(fn packet ->
       String.split(packet, "\n", trim: true)
-      |> Enum.map(fn p ->
-        p |> Jason.decode!()
-      end)
     end)
+    |> List.flatten()
+    |> Enum.map(fn p ->
+      p |> Jason.decode!()
+    end)
+    |> dbg
   end
 
   @doc """
     iex> AdventOfCode.Day13.compare(
-    ...>[[7],[5,[[3,8,9],[1],5],[[7,4],6,[],0],[1,8,[1,5,9]]],[],[[[8,1,8,8,8],5,0,6],5,5,4]],
-    ...>[[7,[[4,6,4,2],[0]],[]]])
-    :lower
+    ...>[[8,6,[],2],[7],[8,0,[7,0],[[5,6],10,[10]]],[3,8,[],[[8,9],4]],[]],
+    ...>[[3,[2,[9,4,4],1],[[3],[],[7,10,6,6],[8,6]]],[[8,5],[2,[],[7]]]])
+    :higher
   """
   def compare([], right) when is_list(right) do
     if Enum.empty?(right), do: :same, else: :lower
@@ -57,11 +59,11 @@ defmodule AdventOfCode.Day13 do
     packet_list =
       args
       |> parse()
-
-    IO.inspect(length(packet_list))
+      |> dbg
 
     results =
       packet_list
+      |> Enum.chunk_every(2)
       |> Enum.reduce([], fn [left, right], acc ->
         [compare(left, right) | acc]
       end)
@@ -74,13 +76,37 @@ defmodule AdventOfCode.Day13 do
     |> Enum.filter(fn {x, _} -> x == :lower or x == :same end)
     |> Enum.map(fn {_, index} -> index end)
     |> Enum.sum()
-    |> dbg(limit: :infinity)
+
+    # |> dbg(limit: :infinity)
   end
 
   @doc """
-    iex> AdventOfCode.Day13.part2("[1,1,3,1,1]\\n[1,1,5,1,1]\\n\\n[[1],[2,3,4]]\\n[[1],4]\\n\\n[9]\\n[[8,7,6]]\\n\\n[[4,4],4,4]\\n[[4,4],4,4,4]\\n\\n[7,7,7,7]\\n[7,7,7]\\n\\n[]\\n[3]\\n\\n[[[]]]\\n[[]]\\n\\n[1,[2,[3,[4,[5,6,7]]]],8,9]\\n[1,[2,[3,[4,[5,6,0]]]],8,9]]]\\n[[2]]\\n[[6]")
-    140
+    # iex> AdventOfCode.Day13.part2("[1,1,3,1,1]\\n[1,1,5,1,1]\\n\\n[[1],[2,3,4]]\\n[[1],4]\\n\\n[9]\\n[[8,7,6]]\\n\\n[[4,4],4,4]\\n[[4,4],4,4,4]\\n\\n[7,7,7,7]\\n[7,7,7]\\n\\n[]\\n[3]\\n\\n[[[]]]\\n[[]]\\n\\n[1,[2,[3,[4,[5,6,7]]]],8,9]\\n[1,[2,[3,[4,[5,6,0]]]],8,9]")
+    # 140
   """
   def part2(args) do
+    packet_list =
+      args
+      |> parse()
+
+    packet_list = [[[2]] | packet_list]
+    packet_list = [[[6]] | packet_list] |> dbg
+
+    result =
+      packet_list
+      |> Enum.sort_by(fn l, r ->
+        {l, r} |> dbg
+
+        case compare(l, r) do
+          :same -> :eq
+          :lower -> :lt
+          :higher -> :gt
+        end
+      end)
+      |> Enum.with_index()
+
+    {_, first} = Enum.find(result, fn x -> x == [[2]] end)
+    {_, second} = Enum.find(result, fn x -> x == [[6]] end)
+    first * second
   end
 end
