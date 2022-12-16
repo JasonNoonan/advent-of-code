8d32051536526dfac278d4b36e35fe532d60c834
Jason Noonan <jason.noonan@pdq.com>
2022-12-16 17:37:29 -0600

    day 16 wip

--- 2022/elixir/lib/advent_of_code/day_16.ex
+++ 2022/elixir/lib/advent_of_code/day_16.ex
@@ -1,5 +1,78 @@
 defmodule AdventOfCode.Day16 do
-  def part1(_args) do
+  def get_weighted_paths(graph, rooms, start_room, time_remaining) do
+    for {name, _} <- rooms, reduce: [] do
+      r ->
+        if name != start_room, do: [Graph.dijkstra(graph, start_room, name) | r], else: r
+    end
+    |> calculate_path_cost(rooms, time_remaining)
+  end
+
+  def get_closed_valves(rooms) do
+    rooms
+    |> Enum.filter(&(&1.state == :closed))
+    |> Enum.sort_by(& &1.rate, :desc)
+  end
+
+  def calculate_path_cost(path_list, rooms, time_remaining) do
+    # TODO: Need to add logic for determining if the cost of opening a valve along the path
+    # is worth an extra round of not having the end valve open
+    for nodes <- path_list do
+      for {node, travel_time} <- Enum.with_index(nodes), room = rooms[node], reduce: {0, 0} do
+        {flow_increase, time_spent} ->
+          acc =
+            if room.state != :closed,
+              do: {flow_increase, time_spent + travel_time},
+              else:
+                (
+                  after_open = time_remaining - travel_time - 1
+                  {flow_increase + room.rate * after_open, time_spent + travel_time + 1}
+                )
+
+          acc
+      end
+    end
+  end
+
+  def open_valve(graph, rooms, target) do
+    # set the state of the target room to :open
+
+    # for each connection of the current room, update
+    # the edge in the graph to set the weight to 0
+  end
+
+  def part1(args) do
+    input_regex =
+      ~r/Valve (?<name>\w+) has flow rate=(?<rate>.+); tunnel[s]? lead[s]? to valve[s]? (?<connections>.*)/
+
+    graph = Graph.new()
+
+    {graph, rooms} =
+      for line <- String.split(args, "\n", trim: true), reduce: {graph, %{}} do
+        {graph, acc} ->
+          m = Regex.named_captures(input_regex, line)
+
+          name = m["name"]
+
+          room = %{
+            rate: String.to_integer(m["rate"]),
+            state: :closed
+          }
+
+          graph =
+            for c <- String.split(m["connections"], ", "), reduce: graph do
+              acc ->
+                Graph.add_edge(acc, name, c, weight: room.rate * -1)
+            end
+
+          room = Map.put(room, :connections, Graph.neighbors(graph, name))
+          acc = Map.put(acc, name, room)
+
+          {graph, acc}
+      end
+
+    {response, list} = Graph.to_dot(graph)
+    IO.write(list)
+    # get_weighted_paths(graph, rooms, "AA", 30) |> dbg
   end
 
   def part2(_args) do
