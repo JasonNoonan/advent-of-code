6e4e8fb17287706fc338153a273ef847058a2078
Jason Noonan <jason.noonan@pdq.com>
2023-12-02 04:11:06 -0600

    day 2 part 2

--- 2023/elixir/lib/advent_of_code/day_02.ex
+++ 2023/elixir/lib/advent_of_code/day_02.ex
@@ -42,5 +42,50 @@ defmodule AdventOfCode.Day02 do
     |> List.flatten()
     |> Enum.sum()
   end
+
+  def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(": ")
+      |> Enum.chunk_every(2)
+      |> Enum.reduce(
+        %{"red" => 0, "green" => 0, "blue" => 0},
+        fn x, acc ->
+          [_game_number, rounds_list] = x
+          get_maximums(rounds_list, acc)
+        end
+      )
+    end)
+    |> List.flatten()
+    |> Enum.map(fn %{"red" => red, "blue" => blue, "green" => green} ->
+      red * blue * green
+    end)
+    |> Enum.sum()
+  end
+
+  def get_maximums(rounds_list, map) do
+    rounds_list
+    |> String.split("; ")
+    |> Enum.reduce(map, fn pull, acc ->
+      String.split(pull, ", ")
+      |> Enum.reduce(acc, fn tile, inner ->
+        tile
+        |> String.split(" ")
+        |> Enum.chunk_every(2)
+        |> Enum.reduce(inner, fn [count, color], madness ->
+          count = String.to_integer(count)
+
+          Map.update!(madness, color, fn curr ->
+            if count > curr do
+              count
+            else
+              curr
+            end
+          end)
+        end)
+      end)
+    end)
   end
 end
