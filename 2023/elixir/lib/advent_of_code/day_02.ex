e9104f3f6b7d040c2869900264f845bc472ba1d3
Jason Noonan <jason.noonan@pdq.com>
2023-12-02 04:00:39 -0600

    day 2 part 1

--- 2023/elixir/lib/advent_of_code/day_02.ex
+++ 2023/elixir/lib/advent_of_code/day_02.ex
@@ -1,7 +1,46 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def impossible?(rounds_list, target) do
+    rounds_list
+    |> String.split("; ")
+    |> Enum.map(fn pull ->
+      String.split(pull, ", ")
+      |> Enum.map(fn tile ->
+        tile
+        |> String.split(" ")
+        |> Enum.chunk_every(2)
+        |> Enum.reject(fn [count, color] ->
+          String.to_integer(count) <= Map.get(target, color)
+        end)
+      end)
+    end)
+    |> List.flatten()
+    |> Enum.any?()
   end
 
-  def part2(_args) do
+  def part1(args) do
+    target = %{"red" => 12, "green" => 13, "blue" => 14}
+
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(": ")
+      |> Enum.chunk_every(2)
+      |> Enum.reduce(
+        [],
+        fn x, acc ->
+          ["Game " <> game_number, rounds_list] = x
+
+          if impossible?(rounds_list, target) do
+            acc
+          else
+            [String.to_integer(game_number) | acc]
+          end
+        end
+      )
+    end)
+    |> List.flatten()
+    |> Enum.sum()
+  end
   end
 end
