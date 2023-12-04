f8ca4fe4db1329c7383e7a7df8041aad1940bcdf
Jason Noonan <jason.noonan@pdq.com>
2023-12-04 07:41:32 -0600

    day 04 part 1

--- 2023/elixir/lib/advent_of_code/day_04.ex
+++ 2023/elixir/lib/advent_of_code/day_04.ex
@@ -1,5 +1,34 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  import AdventOfCode.Helpers
+
+  def score_card(nums) when nums > 1 do
+    Enum.reduce(2..nums, 1, fn _v, acc ->
+      acc * 2
+    end)
+  end
+
+  def score_card(nums) when nums == 1, do: 1
+  def score_card(_nums), do: 0
+
+  def get_number_set(list) do
+    list
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.into(MapSet.new())
+  end
+
+  def part1(args) do
+    args
+    |> lines()
+    |> Enum.reduce(0, fn line, acc ->
+      [_card, winning, mine] = String.split(line, [":", "|"], trim: true)
+      winning = get_number_set(winning)
+      mine = get_number_set(mine)
+
+      winners = MapSet.intersection(winning, mine) |> MapSet.to_list()
+
+      acc + score_card(length(winners))
+    end)
   end
 
   def part2(_args) do
