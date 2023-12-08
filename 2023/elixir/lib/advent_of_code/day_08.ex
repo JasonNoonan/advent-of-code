064924aa3e15e342e6c49888d085eb2ad2f9d5e1
Jason Noonan <jason.noonan@pdq.com>
2023-12-08 13:12:28 -0600

    day 08 part 1

--- 2023/elixir/lib/advent_of_code/day_08.ex
+++ 2023/elixir/lib/advent_of_code/day_08.ex
@@ -1,5 +1,105 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  import AdventOfCode.Helpers
+
+  @doc """
+  iex> split_directions("LRL")
+  %{0 => "L", 1 => "R", 2 => "L"}
+  """
+  def split_directions(directions) do
+    directions
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {d, i}, acc ->
+      Map.put(acc, i, d)
+    end)
+  end
+
+  @doc """
+  iex> next_direction(%{0 => "L", 1 => "R", 2 => "L"}, 0)
+  {"R", 1}
+
+  iex> next_direction(%{0 => "L", 1 => "R", 2 => "L"}, 1)
+  {"L", 2}
+
+  iex> next_direction(%{0 => "L", 1 => "R", 2 => "L"}, 2)
+  {"L", 0}
+  """
+  def next_direction(directions, current) do
+    new = current + 1
+
+    case Map.get(directions, new) do
+      nil ->
+        {Map.get(directions, 0), 0}
+
+      direction ->
+        {direction, new}
+    end
+  end
+
+  @doc """
+  iex> node_to_map("AAA = (BBB, CCC)")
+  %{"AAA" => %{left: "BBB", right: "CCC"}}
+  """
+  def node_to_map(node) do
+    [node, left, right] =
+      node
+      |> String.replace(~r/[\(\)\s)]/, "")
+      |> String.split(["=", ","])
+
+    %{node => %{left: left, right: right}}
+  end
+
+  @doc """
+  iex> convert_nodes("AAA = (BBB, CCC)\\nBBB = (DDD, EEE)")
+  %{
+    "AAA" => %{left: "BBB", right: "CCC"},
+    "BBB" => %{left: "DDD", right: "EEE"}
+  }
+  """
+  def convert_nodes(nodes) do
+    nodes
+    |> lines()
+    |> Enum.reduce(%{}, fn node, acc ->
+      Map.merge(acc, node_to_map(node))
+    end)
+  end
+
+  @doc """
+  iex> move_direction(%{"AAA" => %{left: "BBB", right: "CCC"}}, "AAA", "L")
+  "BBB"
+
+  iex> move_direction(%{"AAA" => %{left: "BBB", right: "CCC"}}, "AAA", "R")
+  "CCC"
+  """
+  def move_direction(map, curr, "L"), do: get_in(map, [curr, :left])
+  def move_direction(map, curr, "R"), do: get_in(map, [curr, :right])
+
+  @doc """
+  iex> find_path(%{0 => "L", 1 => "R"}, %{"AAA" => %{left: "BBB", right: "CCC"}, "BBB" => %{left: "DDD", right: "EEE"}, "EEE" => %{left: "EEE", right: "EEE"}}, "AAA", "EEE")
+  ["EEE", "BBB"]
+  """
+  def find_path(directions, map, current, target) do
+    pos = Map.get(directions, 0)
+    find_path(directions, pos, 0, map, current, target, [])
+  end
+
+  def find_path(_dir, _dv, _dp, _map, current, current, visited), do: visited
+
+  def find_path(directions, d_val, d_pos, map, current, target, visited) do
+    current = move_direction(map, current, d_val)
+    visited = [current | visited]
+    {dir, pos} = next_direction(directions, d_pos)
+    find_path(directions, dir, pos, map, current, target, visited)
+  end
+
+  def part1(args) do
+    [directions, nodes] = String.split(args, "\n\n", trim: true)
+
+    directions = split_directions(directions)
+    nodes = convert_nodes(nodes)
+
+    path = find_path(directions, nodes, "AAA", "ZZZ")
+    length(path)
   end
 
   def part2(_args) do
--- 2023/elixir/test/advent_of_code/day_08_test.exs
+++ 2023/elixir/test/advent_of_code/day_08_test.exs
@@ -2,13 +2,32 @@ defmodule AdventOfCode.Day08Test do
   use ExUnit.Case
 
   import AdventOfCode.Day08
+  doctest AdventOfCode.Day08
 
-  @tag :skip
-  test "part1" do
-    input = nil
+  test "part1_small" do
+    input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"
     result = part1(input)
 
-    assert result
+    assert result == 2
+  end
+
+  test "part1_longer" do
+    input = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"
+    result = part1(input)
+
+    assert result == 6
   end
 
   @tag :skip
