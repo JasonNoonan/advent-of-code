3ef932238c19fca37d987f22a218ddbd976c8d2b
Jason Noonan <jason.noonan@pdq.com>
2023-12-08 23:38:46 -0600

    day 09 part 1

--- 2023/elixir/lib/advent_of_code/day_09.ex
+++ 2023/elixir/lib/advent_of_code/day_09.ex
@@ -1,5 +1,43 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  import AdventOfCode.Helpers
+
+  def get_diff_list([target | _tail] = history) do
+    [
+      target
+      |> slide()
+      |> Enum.map(fn [a, b] -> b - a end)
+      | history
+    ]
+  end
+
+  def reduce_to_zero([], history) do
+    get_diff_list(history)
+    |> reduce_to_zero(history)
+  end
+
+  def reduce_to_zero([diffs | _tail] = history) do
+    if Enum.all?(diffs, fn x -> x == 0 end) do
+      history
+    else
+      history
+      |> get_diff_list()
+      |> reduce_to_zero()
+    end
+  end
+
+  def part1(args) do
+    for line <- lines(args) do
+      history =
+        line
+        |> String.split(" ")
+        |> Enum.map(&String.to_integer/1)
+
+      reduce_to_zero([history])
+      |> Enum.reduce(0, fn h, acc ->
+        List.last(h) + acc
+      end)
+    end
+    |> Enum.sum()
   end
 
   def part2(_args) do
--- 2023/elixir/test/advent_of_code/day_09_test.exs
+++ 2023/elixir/test/advent_of_code/day_09_test.exs
@@ -3,12 +3,13 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
     result = part1(input)
 
-    assert result
+    assert result == 114
   end
 
   @tag :skip
