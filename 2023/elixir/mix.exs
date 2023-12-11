c2f9f1eae99080ca8972941c9aafa4917ba596ef
Jason Noonan <jason.noonan@pdq.com>
2023-12-11 12:23:21 -0600

    refactor: fix ssl error

--- 2023/elixir/mix.exs
+++ 2023/elixir/mix.exs
@@ -22,7 +22,8 @@ defmodule AdventOfCode.MixProject do
   defp deps do
     [
       {:benchee, "~> 1.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ssl_verify_fun, "~> 1.1.7", manager: :rebar3, override: true}
     ]
   end
 end
