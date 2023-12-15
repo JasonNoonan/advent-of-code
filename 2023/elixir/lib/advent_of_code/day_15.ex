1929d7b2ca13cc9c19078ab768d1195359e1b67e
Jason Noonan <jason.noonan@pdq.com>
2023-12-15 14:06:17 -0600

    day 15 part 2

--- 2023/elixir/lib/advent_of_code/day_15.ex
+++ 2023/elixir/lib/advent_of_code/day_15.ex
@@ -21,4 +21,48 @@ defmodule AdventOfCode.Day15 do
     |> Enum.sum()
   end
 
+  def part2(args) do
+    args
+    |> lines()
+    |> Enum.map(&String.split(&1, ",", trim: true))
+    |> List.flatten()
+    |> Enum.reduce(%{}, fn segment, boxes ->
+      %{"label" => label, "op" => op, "focal_length" => focal_length} =
+        Regex.named_captures(~r/(?<label>\w+)(?<op>[-=])(?<focal_length>\d*)/, segment)
+
+      box = hashmap(label)
+
+      if op == "-" do
+        remove_from_box(boxes, box, label)
+      else
+        update_in_box(boxes, box, label, focal_length)
+      end
+    end)
+    |> Enum.map(fn {box, list} ->
+      box = box + 1
+
+      list
+      |> Enum.reverse()
+      |> Enum.with_index(1)
+      |> Enum.map(fn {{_label, focal_length}, index} ->
+        [box, index, String.to_integer(focal_length)] |> Enum.product()
+      end)
+    end)
+    |> List.flatten()
+    |> Enum.sum()
+  end
+
+  def remove_from_box(boxes, box, target) do
+    Map.update(boxes, box, [], fn curr -> List.keydelete(curr, target, 0) end)
+  end
+
+  def update_in_box(boxes, box, target, focal_length) do
+    Map.update(boxes, box, [{target, focal_length}], fn curr ->
+      if List.keymember?(curr, target, 0) do
+        List.keyreplace(curr, target, 0, {target, focal_length})
+      else
+        [{target, focal_length} | curr]
+      end
+    end)
+  end
 end
