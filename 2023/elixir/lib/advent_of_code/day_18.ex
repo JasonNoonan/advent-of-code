d59ac461876515f1c785435af98430a3ab894ad0
Jason Noonan <jason.noonan@pdq.com>
2023-12-18 11:12:38 -0600

    day 18 both parts
    I liked this one, but I lucked out on finding the shoestring and
    pick's theorem stuff early on. I was initially implementing a list
    of all coords, but we really only need the corners and to keep track
    of the perimeter lenght (thanks, Caleb!)

--- 2023/elixir/lib/advent_of_code/day_18.ex
+++ 2023/elixir/lib/advent_of_code/day_18.ex
@@ -1,7 +1,118 @@
 defmodule AdventOfCode.Day18 do
-  def part1(_args) do
+  import AdventOfCode.Helpers
+
+  def expand_points({x, y}, {:down, distance}) do
+    {x, y + distance}
   end
 
-  def part2(_args) do
+  def expand_points({x, y}, {:up, distance}) do
+    {x, y - distance}
+  end
+
+  def expand_points({x, y}, {:left, distance}) do
+    {x - distance, y}
+  end
+
+  def expand_points({x, y}, {:right, distance}) do
+    {x + distance, y}
+  end
+
+  def find_area(points) do
+    shifted = Enum.slide(points, 0, -1)
+
+    points =
+      Enum.zip_with(points, shifted, fn {x, y}, {xn, yn} ->
+        {x * yn, y * xn}
+      end)
+
+    for {a, b} <- points, reduce: 0 do
+      area ->
+        area
+        |> Kernel.+(a)
+        |> Kernel.-(b)
+    end
+    |> abs()
+    |> Kernel.*(0.5)
+    |> dbg
+  end
+
+  def picks_theorem(area, length) do
+    area + length / 2 + 1
+  end
+
+  def part1(args) do
+    runs =
+      args
+      |> lines()
+      |> Enum.map(fn line ->
+        [dir, dist, _color] = String.split(line, " ")
+
+        dir =
+          case dir do
+            "D" -> :down
+            "U" -> :up
+            "L" -> :left
+            "R" -> :right
+          end
+
+        {dir, String.to_integer(dist)}
+      end)
+
+    length =
+      Enum.reduce(runs, 0, fn {_dir, dist}, acc ->
+        acc + dist
+      end)
+
+    {_pos, points} =
+      Enum.reduce(runs, {{0, 0}, []}, fn run, {last_pos, points} ->
+        points_over_run = expand_points(last_pos, run)
+        {points_over_run, [points_over_run | points]}
+      end)
+
+    points
+    |> Enum.reverse()
+    |> List.flatten()
+    |> find_area()
+    |> picks_theorem(length)
+  end
+
+  def part2(args) do
+    runs =
+      args
+      |> lines()
+      |> Enum.map(fn line ->
+        [_dir, _dist, color] = String.split(line, " ")
+
+        color = String.replace(color, ["(", "#", ")"], "") |> String.graphemes()
+
+        dist = Enum.take(color, 5) |> List.to_string() |> String.to_integer(16)
+
+        dir =
+          case List.last(color) do
+            "0" -> :right
+            "1" -> :down
+            "2" -> :left
+            "3" -> :up
+          end
+
+        {dir, dist}
+      end)
+
+    length =
+      Enum.reduce(runs, 0, fn {_dir, dist}, acc ->
+        acc + dist
+      end)
+
+    {_pos, points} =
+      Enum.reduce(runs, {{0, 0}, []}, fn run, {last_pos, points} ->
+        points_over_run = expand_points(last_pos, run)
+        {points_over_run, [points_over_run | points]}
+      end)
+
+    points
+    |> Enum.reverse()
+    |> List.flatten()
+    |> find_area()
+    |> picks_theorem(length)
   end
 end
--- 2023/elixir/test/advent_of_code/day_18_test.exs
+++ 2023/elixir/test/advent_of_code/day_18_test.exs
@@ -3,19 +3,43 @@ defmodule AdventOfCode.Day18Test do
 
   import AdventOfCode.Day18
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)"
     result = part1(input)
 
-    assert result
+    assert result == 62
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)"
     result = part2(input)
 
-    assert result
+    assert result == 952_408_144_115
   end
 end
