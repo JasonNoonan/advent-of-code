84d49920e4c65e028849e75fbcdd749920d2c3bb
Jason Noonan <jason.noonan@pdq.com>
2024-12-01 19:30:48 +0000

    day 01 part 1 & part 2

--- 2024/elixir/lib/advent_of_code/day_01.ex
+++ 2024/elixir/lib/advent_of_code/day_01.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    {left, right} = get_split_list(args)
+
+    left = Enum.sort(left)
+    right = Enum.sort(right)
+
+    Enum.zip_reduce([left, right], [], fn [l, r], acc ->
+      [get_distance(l, r) | acc]
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    {left, right} = get_split_list(args)
+
+    freq = Enum.frequencies(right)
+
+    Enum.reduce(left, 0, fn i, acc ->
+      i * Map.get(freq, i, 0) + acc
+    end)
+  end
+
+  defp get_distance(l, r) when l > r, do: l - r
+  defp get_distance(l, r) when l < r, do: r - l
+  defp get_distance(l, r), do: l - r
+
+  defp get_split_list(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.reduce({[], []}, fn row, {left, right} ->
+      %{"l" => l, "r" => r} = Regex.named_captures(~r/(?<l>\d+)\s+(?<r>\d+)/, row)
+      {[String.to_integer(l) | left], [String.to_integer(r) | right]}
+    end)
   end
 end
--- 2024/elixir/test/advent_of_code/day_01_test.exs
+++ 2024/elixir/test/advent_of_code/day_01_test.exs
@@ -3,19 +3,29 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
+
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3"
+
     result = part2(input)
 
-    assert result
+    assert result == 31
   end
 end
