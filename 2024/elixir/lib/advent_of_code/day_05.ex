97a6c2df7035a8831a239bf4b2ce4883f3468b2e
Jason Noonan <jason.noonan@pdq.com>
2024-12-05 15:11:10 +0000

    day 05 part 2

--- 2024/elixir/lib/advent_of_code/day_05.ex
+++ 2024/elixir/lib/advent_of_code/day_05.ex
@@ -13,7 +13,17 @@ defmodule AdventOfCode.Day05 do
     |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    [rules, updates] =
+      args
+      |> Helpers.divided_lines()
+
+    rules = parse_rules(rules)
+
+    find_failed_orders(updates, rules)
+    |> fix_ordering(rules)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 
   defp parse_rules(rules) do
@@ -49,6 +59,21 @@ defmodule AdventOfCode.Day05 do
         [Enum.at(print_order, middle_index) | acc]
       else
         acc
+      end
+    end)
+  end
+
+  defp find_failed_orders(updates, rules) do
+    Enum.reduce(updates, [], fn row, acc ->
+      print_order =
+        row
+        |> String.split(",")
+        |> Enum.with_index()
+
+      if is_correct_order?(print_order, rules) do
+        acc
+      else
+        [print_order | acc]
       end
     end)
   end
@@ -108,5 +133,40 @@ defmodule AdventOfCode.Day05 do
     else
       index
     end
+  end
+
+  defp fix_ordering(print_orders, rules) do
+    Enum.reduce(print_orders, [], fn row, acc ->
+      page_numbers = Enum.map(row, fn {x, _i} -> x end)
+
+      sorted =
+        Enum.sort(page_numbers, fn x, y ->
+          try_rules(x, y, rules)
+        end)
+
+      middle_index = get_middle_index(sorted)
+
+      [Enum.at(sorted, middle_index) | acc]
+    end)
+  end
+
+  defp try_rules(x, y, rules) do
+    if try_rule(x, y, rules, :x_before) do
+      true
+    else
+      false
+    end
+  end
+
+  defp try_rule(x, y, rules, :x_before) do
+    if Map.has_key?(rules, x) and Map.has_key?(rules[x], :before) and y in rules[x][:before] do
+      true
+    else
+      try_rule(x, y, rules, :y_after)
+    end
+  end
+
+  defp try_rule(x, y, rules, :y_after) do
+    Map.has_key?(rules, y) and Map.has_key?(rules[y], :after) and x in rules[y][:after]
   end
 end
