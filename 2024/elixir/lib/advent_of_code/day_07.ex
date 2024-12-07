805baca6fb1e9ff9d7105d5012e0c1bee14ebddd
Jason Noonan <jason.noonan@pdq.com>
2024-12-07 16:26:54 +0000

    day 07 part 2

--- 2024/elixir/lib/advent_of_code/day_07.ex
+++ 2024/elixir/lib/advent_of_code/day_07.ex
@@ -20,27 +20,58 @@ defmodule AdventOfCode.Day07 do
     end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn row, outer ->
+      [test_value, numbers] = String.split(row, ": ", trim: true)
+      test_value = String.to_integer(test_value)
+
+      results =
+        numbers
+        |> String.split(" ")
+        |> Enum.map(&String.to_integer/1)
+        |> traverse_mathways(true)
+
+      if Enum.any?(results, fn x -> x == test_value end) do
+        outer + test_value
+      else
+        outer
+      end
+    end)
   end
 
-  defp traverse_mathways([init | rest]) do
-    recurse_numbers(rest, [init])
+  defp traverse_mathways([init | rest], concat? \\ false) do
+    recurse_numbers(rest, [init], concat?)
   end
 
-  defp recurse_numbers([y | []], acc) do
+  defp recurse_numbers([y | []], acc, concat?) do
     Enum.map(acc, fn x ->
-      [x + y, x * y]
+      if concat? do
+        [x + y, x * y, concat_numbers(x, y)]
+      else
+        [x + y, x * y]
+      end
     end)
     |> List.flatten()
   end
 
-  defp recurse_numbers([y | rest], acc) do
+  defp recurse_numbers([y | rest], acc, concat?) do
     acc =
       Enum.map(acc, fn x ->
-        [x + y, x * y]
+        if concat? do
+          [x + y, x * y, concat_numbers(x, y)]
+        else
+          [x + y, x * y]
+        end
       end)
       |> List.flatten()
 
-    recurse_numbers(rest, acc)
+    recurse_numbers(rest, acc, concat?)
+  end
+
+  defp concat_numbers(x, y) do
+    (Integer.to_string(x) <> Integer.to_string(y))
+    |> String.to_integer()
   end
 end
--- 2024/elixir/test/advent_of_code/day_07_test.exs
+++ 2024/elixir/test/advent_of_code/day_07_test.exs
@@ -18,11 +18,18 @@ defmodule AdventOfCode.Day07Test do
     assert result == 3749
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"
     result = part2(input)
 
-    assert result
+    assert result == 11387
   end
 end
