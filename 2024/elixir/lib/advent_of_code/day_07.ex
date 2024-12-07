cad0327b7ce78945affb6baea6b1cf5233d3a10d
Jason Noonan <jason.noonan@pdq.com>
2024-12-07 16:19:12 +0000

    day 07 part 1

--- 2024/elixir/lib/advent_of_code/day_07.ex
+++ 2024/elixir/lib/advent_of_code/day_07.ex
@@ -1,7 +1,46 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn row, outer ->
+      [test_value, numbers] = String.split(row, ": ", trim: true)
+      test_value = String.to_integer(test_value)
+
+      results =
+        numbers
+        |> String.split(" ")
+        |> Enum.map(&String.to_integer/1)
+        |> traverse_mathways()
+
+      if Enum.any?(results, fn x -> x == test_value end) do
+        outer + test_value
+      else
+        outer
+      end
+    end)
   end
 
   def part2(_args) do
+  end
+
+  defp traverse_mathways([init | rest]) do
+    recurse_numbers(rest, [init])
+  end
+
+  defp recurse_numbers([y | []], acc) do
+    Enum.map(acc, fn x ->
+      [x + y, x * y]
+    end)
+    |> List.flatten()
+  end
+
+  defp recurse_numbers([y | rest], acc) do
+    acc =
+      Enum.map(acc, fn x ->
+        [x + y, x * y]
+      end)
+      |> List.flatten()
+
+    recurse_numbers(rest, acc)
   end
 end
--- 2024/elixir/test/advent_of_code/day_07_test.exs
+++ 2024/elixir/test/advent_of_code/day_07_test.exs
@@ -3,12 +3,19 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"
     result = part1(input)
 
-    assert result
+    assert result == 3749
   end
 
   @tag :skip
