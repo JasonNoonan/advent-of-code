4ec8213a52a0f1ea41648a7bc0f412b7c1050831
Jason Noonan <jason.noonan@pdq.com>
2024-12-08 22:07:39 +0000

    day 08 part 2 (WIP)
    oof
    
    Too low with 1119
    
    Correct answer after stealing Calbe's branch is 1184. Gotta figure out
    where I'm going wrong.

--- 2024/elixir/lib/advent_of_code/day_08.ex
+++ 2024/elixir/lib/advent_of_code/day_08.ex
@@ -28,10 +28,55 @@ defmodule AdventOfCode.Day08 do
     |> Enum.count(fn {x, y} -> Map.get(map, {x, y}) end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    map =
+      args
+      |> Helpers.lines()
+      |> Enum.map(&String.graphemes/1)
+      |> Helpers.list_to_map()
+
+    symbols_map =
+      Enum.reject(map, fn {_coords, symbol} -> symbol == "." end)
+
+    symbol_slopes = get_all_symbol_slopes(symbols_map) |> MapSet.to_list()
+
+    Enum.reduce(map, MapSet.new(), fn {{x, y}, _symbol}, acc ->
+      if Enum.reduce_while(symbol_slopes, false, fn
+           {:infinity, {x1, _y1}, {_x2, _y2}}, inner ->
+             if x == x1, do: {:halt, true}, else: {:cont, inner}
+
+           {m, b}, inner ->
+             if Helpers.in_line?({x, y}, m, b), do: {:halt, true}, else: {:cont, inner}
+         end),
+         do: MapSet.put(acc, {x, y}),
+         else: acc
+    end)
+    |> MapSet.to_list()
+    |> Enum.count()
   end
 
   defp map_antinode({x1, y1}, {x2, y2}) do
     {x1 + (x1 - x2), y1 + (y1 - y2)}
+  end
+
+  defp get_all_symbol_slopes(symbols_map) do
+    Enum.reduce(symbols_map, MapSet.new(), fn {coords, symbol}, slopes ->
+      Enum.reduce(symbols_map, slopes, fn
+        {^coords, ^symbol}, acc ->
+          acc
+
+        {target, ^symbol}, slopes ->
+          results =
+            case Helpers.find_slope(coords, target) do
+              {:ok, {m, b}} -> {m, b}
+              {:error, :undefined} -> {:infinity, coords, target}
+            end
+
+          MapSet.put(slopes, results)
+
+        {_other, _other_symbol}, acc ->
+          acc
+      end)
+    end)
   end
 end
