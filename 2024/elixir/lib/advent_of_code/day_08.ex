3bcdf261d1c2589460693ac57a8f0d3081e8b947
Jason Noonan <jason.noonan@pdq.com>
2024-12-08 14:45:13 -0600

    day 08 part 1 (fixed)
    went so far in the wrong direction lmao

--- 2024/elixir/lib/advent_of_code/day_08.ex
+++ 2024/elixir/lib/advent_of_code/day_08.ex
@@ -5,56 +5,33 @@ defmodule AdventOfCode.Day08 do
     map =
       args
       |> Helpers.lines()
+      |> Enum.map(&String.graphemes/1)
       |> Helpers.list_to_map()
 
-    symbols_map = Enum.reject(map, fn {coords, symbol} -> symbol != "." end)
-    symbol_slopes = get_all_symbol_slopes(symbols_map)
+    symbols_map = Enum.reject(map, fn {_coords, symbol} -> symbol == "." end)
 
-    # iterate over full map and for each location, find the slope of the
-    # current location and a symbol from the symbol_slopes map. If they match,
-    # determine if the current point is twice as far from point A as it is from
-    # point B, or twice as far from point B as it is from point A
+    Enum.reduce(symbols_map, [], fn {coords, symbol}, antinodes ->
+      Enum.reduce(symbols_map, antinodes, fn
+        {^coords, ^symbol}, acc ->
+          acc
 
-    # If so, we have an antinode. Otherwise, move on. Reminder that even
-    # locations that have a current symbol can be antinodes.
+        {target, ^symbol}, a_nodes ->
+          antinode = map_antinode(coords, target)
+
+          [antinode | a_nodes]
+
+        {_other, _other_symbol}, acc ->
+          acc
+      end)
+    end)
+    |> Enum.uniq()
+    |> Enum.count(fn {x, y} -> Map.get(map, {x, y}) end)
   end
 
   def part2(_args) do
   end
 
-  defp get_all_symbol_slopes(symbols_map) do
-    Enum.reduce(symbols_map, %{}, fn {{x, y}, symbol}, acc ->
-      Enum.filter(symbols_map, fn
-        {coords, ^symbol} ->
-          # matching symbols and we haven't already calculated either direction
-          # of a->b or b->a for this line
-          # and not our same, current symbol location
-          not line_already_calculated?(symbol, {x, y}, coords, acc) and
-            {x, y} != coords
-
-        {_coords, _symbol} ->
-          false
-      end)
-      |> Enum.reduce(acc, fn {{x2, y2}, _sym}, inner ->
-        case Helpers.find_slope({x, y}, {x2, y2}) do
-          {:ok, {m, b}} ->
-            Map.put(inner, {symbol, {x, y}}, {m, b})
-
-          {:error, :undefined} ->
-            Map.put(inner, {symbol, {x, y}, {:infinity, 0}})
-
-          _else ->
-            inner
-        end
-      end)
-    end)
-  end
-
-  defp line_already_calculated?(symbol, a, b, map) do
-    a_list = Map.get(map, {symbol, a})
-    b_list = Map.get(map, {symbol, b})
-
-    (not is_nil(a_list) and {symbol, b} in a_list) or
-      (not is_nil(b_list) and {symbol, a} in b_list)
+  defp map_antinode({x1, y1}, {x2, y2}) do
+    {x1 + (x1 - x2), y1 + (y1 - y2)}
   end
 end
