2bd04c7cf66e42cc507b4412924dbf1c8fb81bd6
Jason Noonan <jason.noonan@pdq.com>
2024-12-10 13:16:05 +0000

    day 10 part 1

--- 2024/elixir/lib/advent_of_code/day_10.ex
+++ 2024/elixir/lib/advent_of_code/day_10.ex
@@ -1,7 +1,50 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_args) do
+  alias AdventOfCode.Helpers
+
+  def part1(args) do
+    map = parse_map(args)
+    start_locations = get_positions_by_height(map, 0)
+    targets = get_positions_by_height(map, 9)
+
+    for {th, _v} <- start_locations, reduce: 0 do
+      acc ->
+        for {tar, _val} <- targets do
+          cost =
+            Helpers.dijkstras(map, th, tar, fn
+              cost, start, next when next - start == 1 ->
+                cost
+
+              _cost, _start, _next ->
+                :infinity
+            end)
+
+          # cost 0 means we found a happy path
+          if cost == 0, do: 1, else: 0
+        end
+        |> Enum.sum()
+        |> Kernel.+(acc)
+    end
   end
 
   def part2(_args) do
+  end
+
+  defp parse_map(args) do
+    args
+    |> Helpers.lines()
+    |> Enum.map(fn x ->
+      x
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Helpers.list_to_map()
+  end
+
+  defp get_positions_by_height(map, target_height) do
+    map
+    |> Enum.filter(fn
+      {_coords, ^target_height} -> true
+      _else -> false
+    end)
   end
 end
--- 2024/elixir/test/advent_of_code/day_10_test.exs
+++ 2024/elixir/test/advent_of_code/day_10_test.exs
@@ -3,12 +3,18 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"
     result = part1(input)
 
-    assert result
+    assert result == 36
   end
 
   @tag :skip
